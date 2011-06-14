3f65e634e638a1594e0ef6798150f24d3a92811e
Daniel Temme <dtemme@gmail.com>
2011-06-14 21:01:25 +0200

    wasting time reading the wrong api docs

--- ruby/day2/grep.rb
+++ ruby/day2/grep.rb
@@ -1,3 +1,9 @@
 #!/usr/bin/ruby
 
-File.foreach(ARGV[0]) {|line| puts line }
+ARGV.size == 2 or puts "usage: $0 <regexp> <filename>" && exit
+
+pos = 0
+File.foreach(ARGV[1]) do |line|
+	pos += 1
+	puts " #{pos}: #{line}" if line.match(ARGV[0])
+end
