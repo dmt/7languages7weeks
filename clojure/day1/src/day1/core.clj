2aa09e99c19ab2cd89c152b64afda31e839d58e9
Daniel Temme <dtemme@gmail.com>
2011-07-20 17:45:09 +0200

    collection-type with cond; don't like the repeated col param

--- clojure/day1/src/day1/core.clj
+++ clojure/day1/src/day1/core.clj
@@ -2,3 +2,10 @@
 
 (defn big [st n]
   (< n (count st)))
+
+(defn collection-type [col]
+  (cond (set? col) :set
+        (map? col) :map
+        (list? col) :list
+        (vector? col) :vector
+        (seq? col) :seq))
--- clojure/day1/test/day1/test/core.clj
+++ clojure/day1/test/day1/test/core.clj
@@ -3,7 +3,14 @@
   (:use [clojure.test]))
 
 (deftest big-if-string-is-longer-than-n
-  (is (= false (big "" 1)))
-  (is (= false (big "xx" 2)))
-  (is (= true (big "xx" 1))))
+  (is (false? (big "" 1)))
+  (is (false? (big "xx" 2)))
+  (is (true? (big "xx" 1))))
+
+(deftest coll-type-returns-correct-name
+  (is (= :list (collection-type '())))
+  (is (= :map (collection-type {})))
+  (is (= :vector (collection-type [])))
+  (is (= :set (collection-type #{}))))
+
 
