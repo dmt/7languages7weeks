53346c5c8998d7a89eeae69ccbf1fc66eb469b6b
Daniel Temme <dtemme@gmail.com>
2011-07-22 14:34:55 +0200

    write out SimpleCompass
    need the explicit import to use defrecord types outside their own namesp
    ace

---
+++ clojure/day2/test/day2/test/compass.clj
@@ -0,0 +1,13 @@
+(ns day2.test.compass
+  (:use [day2.compass])
+  (:use [clojure.test])
+  (:import [day2.compass SimpleCompass]))
+
+(deftest turn-changes-between-4-directions
+  (is (= 0 (turn 0 0)))
+  (is (= 3 (turn 1 2)))
+  (is (= 0 (turn 1 3))))
+
+(deftest simple-compass
+  (testing "creation"
+    (is (= :north (direction (SimpleCompass. 0))))))
