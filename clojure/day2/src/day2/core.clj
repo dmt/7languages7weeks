e7f0e517773d0b908474355f8960be35f619f3ae
Daniel Temme <dtemme@gmail.com>
2011-07-22 15:37:07 +0200

    unless macro

--- clojure/day2/src/day2/core.clj
+++ clojure/day2/src/day2/core.clj
@@ -3,3 +3,7 @@
 (defn- fib-pair [[current next]] [next (+ current next)])
 (defn fib [pos]
   (nth (map first (iterate fib-pair [1 1])) (- pos 1)))
+
+
+(defmacro unless [test body]
+  (list 'if (list 'not test) body))
--- clojure/day2/test/day2/test/core.clj
+++ clojure/day2/test/day2/test/core.clj
@@ -7,3 +7,7 @@
   (is (= 1 (fib 2)))
   (is (= 2 (fib 3)))
   (is (= 13 (fib 7))))
+
+(deftest unless-macro
+  (is (= :foo (unless false :foo)) "execute body if test fails")
+  (is (nil? (unless true :foo)) "do nothing is test is true"))
